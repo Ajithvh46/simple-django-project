bbff668848ca10759f2f5c6fe94241681cddbbd7
Ajith VH <ajithvh46@gmail.com>
2025-01-17 00:46:42 +0530

    list file updated

--- movies/movie/views.py
+++ movies/movie/views.py
@@ -3,39 +3,41 @@ from django.shortcuts import render
 # Create your views here.
 
 def movie_review(request):
+    return render(request,'index.html')
+
+def create_view(request):
+    return render(request,'create.html')
+
+def list_view(request):
     movie_details={
         'movie_data':[{
         'title':'Interstellar',
         'year':2014,
         'summary':'Science fiction',
-        'success':True
+        'success':True,
         },
         {'title':'Inception',
         'year':2010,
         'summary':'Science fiction',
-        'success':True
+        'success':True,
         },
         {
         'title':'Marco',
         'year':2024,
         'summary':'Action movie',
-        'success':True
+        'success':True,
         },
         {
         'title':'Goat',
         'year':2024,
-        
-        'success':False
+        'summary':'',
+        'success':False,
         }
         ]
         
     }
-    return render(request,'index.html',movie_details)
-def create_view(request):
-    return render(request,'create.html')
 
-def list_view(request):
-    return render(request,'list.html')
+    return render(request,'list.html',movie_details)
 
 def edit_view(request):
     return render(request,'edit.html')
